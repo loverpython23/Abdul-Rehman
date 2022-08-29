8ce030c32d03ed6f55be3ca7ab193d35934a3883
loverpython23 <112357021+loverpython23@users.noreply.github.com>
2022-08-29 05:20:25 +0500

    Add files via upload

---
+++ port.js
@@ -0,0 +1,26 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navList = document.querySelector('.nav-list')
+burger.addEventListener('click', () => {
+  navList.classList.toggle('v-class-resp')
+  navbar.classList.toggle('h-nav-resp')
+})
